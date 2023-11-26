3ae238050159db208c57b73365b57b66ecdf8fb5
Kai-Yuan <32070909+B0544218@users.noreply.github.com>
2023-11-26 14:55:51 +0800

    Create Main.java

---
+++ basic_review/4. Date/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+import java.time.LocalDateTime; // import the LocalDate class
+import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDateTime myDateObj = LocalDateTime.now();
+        System.out.println("Before formatting: " + myDateObj);
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        String formattedDate = myDateObj.format(myFormatObj);
+        System.out.println("After formatting: " + formattedDate);
+    }
+}
